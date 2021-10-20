e8bacf21ac51e5aa551b03865ab557a38c29016f
turkeysui <suiminyan@gmail.com>
2021-10-20 21:51:33 +0800

    clear all deprecated codes

--- okex/main.py
+++
@@ -1,9 +0,0 @@
-from okex.websocket import subscribe_without_login, subscribe
-from const import url, api_key, passphrase, secret_key
-import asyncio
-
-channels = ["swap/depth5:BTC-USD-SWAP"]
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(subscribe(url, api_key, passphrase, secret_key, channels))
-loop.close()
