b5c402fb38e65ac02fb69e2d8bf2f6bf2519fd17
donkeysui <85976180+donkeysui@users.noreply.github.com>
2021-10-27 19:14:27 +0800

    1. nearly complete single-thread FileWriter Class 2. set some consts at const.py

---
+++ data_listener/okexV5_swap_kline/bar_fetch.py
@@ -0,0 +1,60 @@
+import datetime
+import ccxt
+import pandas as pd
+from influxdb import DataFrameClient
+import sys
+import json
+import time
+
+
+class BarFetcher:
+
+    def __init__(self, symbols, measurement=None):
+
+        self._get_threshold = 0.1
+        self.conn = ccxt.okex()
+        self.dbclient = DataFrameClient()
+        self.symbols = symbols
+        if measurement:
+            self.influx_measurement = measurement
+        else:
+            self.influx_measurement = 'swap_kline'
+        self.convert = datetime.datetime.fromtimestamp
+
+    def fetch(self, symbol):
+
+        res = self.conn.fetchOHLCV(symbol)
+        resdf = pd.DataFrame(res, columns=['time', 'o', 'h', 'l', 'c', 'v'])
+        resdf.time = resdf.time / 1000
+        resdf.time = resdf.time.apply(self.convert)
+        resdf.index = resdf.time
+        resdf.drop('time', axis=1, inplace=True)
+        resdf['symbol'] = symbol
+
+        return resdf
+
+    def write_influx(self, data):
+
+        self.dbclient.write_points(data,
+                                   measurement=self.influx_measurement,
+                                   tag_columns=['symbol'])
+
+    def all_fetch_and_write(self):
+
+        for symbol in self.symbols:
+            self.write_influx(self.fetch(symbol))
+            time.sleep(self._get_threshold)
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) > 1:
+        config_file_name = sys.argv[1]
+
+    with open(config_file_name) as file:
+        swap_symbols = json.load(file)['symbols']
+
+    Fetcher = BarFetcher(symbols=swap_symbols)
+    Fetcher.all_fetch_and_write()
+
+
